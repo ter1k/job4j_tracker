e49d0d07e96cb3ed7328e8c7d8bc2d0d775c73e0
ter1k <terletskynick@gmail.com>
2022-06-24 20:24:34 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,13 +2,35 @@ package ru.job4j.oop;
 
 public class Cat {
 
+    private String food;
+    private String name;
+
     public String sound() {
         return "may-may";
     }
 
+    public void show() {
+        System.out.println(this.name + "; " + this.food);
+    }
+
+    public void feed(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("This here is Gav's food.");
+        Cat gav = new Cat();
+        gav.feed("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("And this here on the other hand belongs to Black.");
+        Cat black = new Cat();
+        black.feed("fish");
+        black.giveNick("Black");
+        black.show();
     }
 }
